9a5c27d78fc90e3381910697e6989614ce0e67b2
RachnaDodia <rachna.dodia@gmail.com>
2024-11-13 11:18:34 +0530

    Update mysql_script.sql

--- mysql_script.sql
+++ mysql_script.sql
@@ -1,6 +1,4 @@
-CREATE TABLE IF NOT EXISTS rbl_table (           
-  message varchar(255) UNIQUE NOT NULL         
-  ) ENGINE=MyISAM DEFAULT CHARSET=utf8;         
-  INSERT IGNORE INTO rbl_table(message) VALUES('Sample Data inserted');
-  INSERT IGNORE INTO rbl_table(message) VALUES('For RBL Demo');         
-  INSERT IGNORE INTO rbl_table(message) VALUES('sample Data ends')
+INSERT INTO apps(appid,name,ip) VALUES(5,"app1 banking","10.2.43.30");
+INSERT INTO apps(appid,name,ip) VALUES(6,"app2 banking","3.40.43.44");
+INSERT INTO apps(appid,name,ip) VALUES(7,"app3 banking","192.30.33.30");
+INSERT INTO apps(appid,name,ip) VALUES(4,"app5 banking","144.45.22.44");
